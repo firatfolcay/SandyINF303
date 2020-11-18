c701b8c172dc0d73ffa6cafd48910b8f866b2846
Arman Yvz <e170503023@stud.tau.edu.tr>
2020-11-18 16:37:59 +0300

    fixed some options for floating action buttons

--- app/src/main/java/sandy/android/assistant/MainActivity.java
+++ app/src/main/java/sandy/android/assistant/MainActivity.java
@@ -8,16 +8,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     RecyclerView recyclerView;
     View view;
-    Button button_open_note_editor = (Button) findViewById(R.id.button_open_note_editor);  //button to open note editor screen
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_open_note_editor = (Button) findViewById(R.id.button_open_note_editor);
 
         button_open_note_editor.setOnClickListener(new View.OnClickListener() {
             @Override
