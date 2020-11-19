52a400efa5a899b46932002b582a1593ee83decf
Arman Yvz <e170503023@stud.tau.edu.tr>
2020-11-19 21:28:34 +0300

    add and delete image functions inside edittext were added.

--- app/src/main/java/sandy/android/assistant/NoteEditorController.java
+++ app/src/main/java/sandy/android/assistant/NoteEditorController.java
@@ -1,7 +1,23 @@
 package sandy.android.assistant;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ImageSpan;
 import android.view.View;
+import android.widget.EditText;
+import java.io.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -9,6 +25,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.*;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 public class NoteEditorController extends AppCompatActivity {
 
@@ -21,6 +39,9 @@ public class NoteEditorController extends AppCompatActivity {
     FloatingActionButton fab_noteeditor_options_addimage;
     FloatingActionButton fab_noteeditor_options_timer;
     FloatingActionButton fab_noteeditor_options_calendar;
+    EditText noteeditor_multiline_text;
+    int multiline_height;
+    int multiline_width;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +53,7 @@ public class NoteEditorController extends AppCompatActivity {
         fab_noteeditor_options_addimage = (FloatingActionButton) findViewById(R.id.fab_noteeditor_options_addimage);
         fab_noteeditor_options_timer = (FloatingActionButton) findViewById(R.id.fab_noteeditor_options_timer);
         fab_noteeditor_options_calendar = (FloatingActionButton) findViewById(R.id.fab_noteeditor_options_calendar);
+        noteeditor_multiline_text = (EditText) findViewById(R.id.noteeditor_multiline_text);
 
         fab_noteeditor_options.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +65,34 @@ public class NoteEditorController extends AppCompatActivity {
                 }
             }
         });
+
+        fab_noteeditor_options_addimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Uri targetUri = data.getData();
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                bitmap = Bitmap.createScaledBitmap(bitmap, (int) (0.5 * noteeditor_multiline_text.getWidth()), (int) (0.5 * noteeditor_multiline_text.getHeight()), false);
+
+                Drawable d = new BitmapDrawable(getResources(), bitmap);
+                addImageInEditText(d);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void showFABMenu(){
@@ -67,5 +117,40 @@ public class NoteEditorController extends AppCompatActivity {
         fab_noteeditor_options_timer.setVisibility(View.INVISIBLE);
         fab_noteeditor_options_calendar.setVisibility(View.INVISIBLE);
     }
+
+
+    private void addImageInEditText(Drawable drawable) {
+        noteeditor_multiline_text = (EditText) findViewById(R.id.noteeditor_multiline_text);
+
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+
+        int selectionCursorPos = noteeditor_multiline_text.getSelectionStart();
+        noteeditor_multiline_text.getText().insert(selectionCursorPos, ".");
+        selectionCursorPos = noteeditor_multiline_text.getSelectionStart();
+        SpannableStringBuilder builder = new SpannableStringBuilder(noteeditor_multiline_text.getText());
+        int startPos = selectionCursorPos - ".".length();
+        builder.setSpan(new ImageSpan(drawable), startPos, selectionCursorPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        noteeditor_multiline_text.setText(builder);
+        noteeditor_multiline_text.setSelection(selectionCursorPos);
+    }
+
+    private void deleteImageFromEditText() {
+        String msgEditText = noteeditor_multiline_text.getText().toString();
+        System.out.println("text length : " + msgEditText.length());
+        if (msgEditText.length() > 0) {
+            int selectionCursorPos = noteeditor_multiline_text.getSelectionStart();
+            int endPosition = noteeditor_multiline_text.getText().length();
+            System.out.println("cursor Pos: " + selectionCursorPos + " endPosition : " + endPosition);
+
+            if (selectionCursorPos > 0) {
+                int deletingObjectStartPos = selectionCursorPos - 1;
+                noteeditor_multiline_text.getText().delete(deletingObjectStartPos, selectionCursorPos);
+                noteeditor_multiline_text.setSelection(deletingObjectStartPos);
+            }
+        } else {
+            noteeditor_multiline_text.setText("");
+        }
+    }
+
 
 }
