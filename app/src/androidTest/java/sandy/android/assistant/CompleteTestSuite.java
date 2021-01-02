a46f0ee8b540b8968b5ca69bb84fb43c43d6aa96
Yunus Emre Zobi <y.emrezobi@gmail.com>
2021-01-02 21:39:57 +0300

    add test suites

---
+++ app/src/androidTest/java/sandy/android/assistant/CompleteTestSuite.java
@@ -0,0 +1,11 @@
+package sandy.android.assistant;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        NoteTestSuite.class
+})
+public class CompleteTestSuite {
+}
