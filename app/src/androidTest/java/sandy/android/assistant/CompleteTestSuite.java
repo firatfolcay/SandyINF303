31f1707db16f4a8a6dd23a355971dfa853aa0c75
Yunus Emre Zobi <y.emrezobi@gmail.com>
2021-01-02 22:33:43 +0300

    add test to edit notebook

--- app/src/androidTest/java/sandy/android/assistant/CompleteTestSuite.java
+++ app/src/androidTest/java/sandy/android/assistant/CompleteTestSuite.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        NoteTestSuite.class
+        NoteTestSuite.class,
+        NotebookTestSuite.class
 })
 public class CompleteTestSuite {
 }
--- app/src/androidTest/java/sandy/android/assistant/NotebookTestSuite.java
+++ app/src/androidTest/java/sandy/android/assistant/NotebookTestSuite.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        CreateNotebookTest.class
+        CreateNotebookTest.class,
+        EditNotebookTest.class
 })
 public class NotebookTestSuite {
 }
